7abcb295b598f408fa7586bc4a1691f5be025cc6
Akhil Wali <akhil.wali.10@gmail.com>
2016-12-28 18:29:51 +1300

    Init repo

---
+++ LazyList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LazyList
+{
+	class LazyList<T> : IEnumerable<T>, IEnumerable
+	{
+		T _first;
+		Lazy<LazyList<T>> _rest;
+
+		public LazyList(T first, Lazy<LazyList<T>> rest)
+		{
+			_first = first;
+			_rest = rest;
+		}
+
+		public T First { get { return _first; } }
+
+		public LazyList<T> Rest { get { return _rest.Value; } }
+
+		public static LazyList<U> Iterate<U>(Func<U, U> f, U x)
+		{
+			return new LazyList<U>(x, new Lazy<LazyList<U>>(delegate
+				{
+					return Iterate(f, f(x));
+				}));
+		}
+
+		public static LazyList<U> Repeat<U>(U x)
+		{
+			return new LazyList<U>(x, new Lazy<LazyList<U>>(delegate
+				{
+					return Repeat(x);
+				}));
+		}
+
+		#region IEnumerable[T] implementation
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			return new LazyListEnumerator<T>(this);
+		}
+		#endregion
+
+		#region IEnumerable implementation
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return new LazyListEnumerator<T>(this);
+		}
+		#endregion
+	}
+}
+
---
+++ LazyListEnumerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LazyList
+{
+	class LazyListEnumerator<T> : IEnumerator<T>, IEnumerator
+	{
+		LazyList<T> _next, _curr = null;
+
+		public LazyListEnumerator (LazyList<T> parent)
+		{
+			_next = parent;
+		}
+
+		T Current {
+			get {
+				if (_curr == null)
+					throw new InvalidOperationException ();
+
+				return _curr.First;
+			}
+		}
+
+		#region IEnumerator<T> implementation
+
+		T IEnumerator<T>.Current { get { return Current; } }
+
+		#endregion
+
+		#region IEnumerator implementation
+
+		bool IEnumerator.MoveNext ()
+		{
+			_curr = _next;
+			_next = _next.Rest;
+
+			return _curr != null;
+		}
+
+		void IEnumerator.Reset ()
+		{
+			throw new NotSupportedException ("Cannot reset a LazyList");
+		}
+
+		object IEnumerator.Current { get { return Current; } }
+
+		#endregion
+
+		#region IDisposable implementation
+
+		void System.IDisposable.Dispose ()
+		{
+		}
+
+		#endregion
+	}
+
+
+}
+
